9b5012ca14aefe8a653f246cfe2e3efc31f4632f
Jeremy Smith <jsmith@mca.net>
2022-02-23 15:32:32 -0500

    updates to emailer.

--- source/Services.Email/EmailAttachment.cs
+++ source/Services.Email/EmailAttachment.cs
@@ -1,25 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
-namespace Services.Email
+namespace Services.Email;
+
+/// <summary>
+/// Container for files that will be attached to an email.
+/// </summary>
+public class EmailAttachment
 {
     /// <summary>
-    /// Container for files that will be attached to an email.
+    /// Name of the file when attached.
     /// </summary>
-    public class EmailAttachment
-    {
-        /// <summary>
-        /// Name of the file when attached.
-        /// </summary>
-        public string FileName { get; set; }
+    public string FileName { get; set; }
 
-        /// <summary>
-        /// Binary data for the attachment.
-        /// </summary>
-        public Stream Data { get; set; }
-    }
+    /// <summary>
+    /// Binary data for the attachment.
+    /// </summary>
+    public Stream Data { get; set; }
 }
