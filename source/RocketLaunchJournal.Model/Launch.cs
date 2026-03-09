2536309ebc61b0e1637caeae51e9c23535353f48
Jeremy Smith <jsmith@mca.net>
2026-03-09 05:42:10 -0400

    Cleaned up some warnings.

--- source/RocketLaunchJournal.Model/Launch.cs
+++ source/RocketLaunchJournal.Model/Launch.cs
@@ -1,4 +1,5 @@
-﻿using RocketLaunchJournal.Model.Constants;
+﻿using Microsoft.EntityFrameworkCore;
+using RocketLaunchJournal.Model.Constants;
 using System;
 using System.ComponentModel.DataAnnotations;
 
@@ -24,33 +25,43 @@ public class LaunchBase
   public string? Motors { get; set; }
 
   [Display(ShortName = "Alt")]
+  [Precision(18,6)]
   public decimal? Altitude { get; set; }
 
   [Display(Name = "Top Speed", ShortName = "TS")]
+  [Precision(18, 6)]
   public decimal? TopSpeed { get; set; }
 
   [Display(Name = "Burn Time", ShortName = "Burn")]
+  [Precision(18, 6)]
   public decimal? BurnTime { get; set; }
 
   [Display(Name = "Peak Acceleration", ShortName = "PAcc")]
+  [Precision(18, 6)]
   public decimal? PeakAcceleration { get; set; }
 
   [Display(Name = "Average Acceleration", ShortName = "AAcc")]
+  [Precision(18, 6)]
   public decimal? AverageAcceleration { get; set; }
 
   [Display(Name = "Coast To Apogee Time", ShortName = "C2AP")]
+  [Precision(18, 6)]
   public decimal? CoastToApogeeTime { get; set; }
 
   [Display(Name = "Apogee To Ejection Time", ShortName = "A2EJ")]
+  [Precision(18, 6)]
   public decimal? ApogeeToEjectionTime { get; set; }
 
   [Display(Name = "Ejection Altitude", ShortName = "EAlt")]
+  [Precision(18, 6)]
   public decimal? EjectionAltitude { get; set; }
 
   [Display(Name = "Descent Speed", ShortName = "Desc")]
+  [Precision(18, 6)]
   public decimal? DescentSpeed { get; set; }
 
   [Display(ShortName = "Dura")]
+  [Precision(18, 6)]
   public decimal? Duration { get; set; }
 
   [Display(Name = "Recovery Notes")]
--- source/RocketLaunchJournal.Model/Rocket.cs
+++ source/RocketLaunchJournal.Model/Rocket.cs
@@ -1,4 +1,5 @@
-﻿using RocketLaunchJournal.Model.Constants;
+﻿using Microsoft.EntityFrameworkCore;
+using RocketLaunchJournal.Model.Constants;
 using RocketLaunchJournal.Model.UserIdentity;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -14,12 +15,15 @@ public class RocketBase
   public string? Name { get; set; }
 
   [Display(Name = "Weight", ShortName = "WT")]
+  [Precision(18, 6)]
   public decimal? Weight { get; set; }
 
   [Display(Name = "Diameter", ShortName = "Dia")]
+  [Precision(18, 6)]
   public decimal? Diameter { get; set; }
 
   [Display(Name = "Length", ShortName = "Len")]
+  [Precision(18, 6)]
   public decimal? Length { get; set; }
 
   [Display(Name = "Recovery")]
@@ -27,21 +31,27 @@ public class RocketBase
   public string? Recovery { get; set; }
 
   [Display(Name= "Tube Length For Apogee Charge")]
+  [Precision(18, 6)]
   public decimal? TubeLengthForApogeeCharge { get; set; }
 
   [Display(Name = "Black Powder for Apogee", ShortName = "BPAP")]
+  [Precision(18, 6)]
   public decimal? BlackPowderForApogee { get; set; }
 
   [Display(Name = "Tube Length For Main Charge")]
+  [Precision(18, 6)]
   public decimal? TubeLengthForMainCharge { get; set; }
 
   [Display(Name = "Black Powder for Main", ShortName = "BPM")]
+  [Precision(18, 6)]
   public decimal? BlackPowderForMain { get; set; }
 
   [Display(Name ="Center of Gravity")]
+  [Precision(18, 6)]
   public decimal? CenterOfGravity { get; set; }
 
   [Display(Name = "Center of Preassure")]
+  [Precision(18, 6)]
   public decimal? CenterOfPreassure { get; set; }
 
   public int UserId { get; set; }
